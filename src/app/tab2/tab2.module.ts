0c0b174401dd82a4fd77f99631d42863fee53186
Ashwin <ashwinlaly.com>
2021-01-23 13:49:11 +0530

    listing changes

--- src/app/tab2/tab2.module.ts
+++ src/app/tab2/tab2.module.ts
@@ -19,34 +19,5 @@ import { Tab2PageRoutingModule } from './tab2-routing.module';
   declarations: [Tab2Page]
 })
 export class Tab2PageModule {
-  text = "!23"
-  
-  timeline = [{
-    date: new Date(),
-    title: "Awesome picture",
-    author: "John Mybeweeg",
-    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
-    type: "picture"
-  }, {
-    date: new Date(),
-    title: "Look at my video!",
-    author: "Miranda Smith",
-    text: "Lorem ipsum dolor sit amet",
-    type: "video"
 
-  }, {
-    date: new Date(),
-    title: "I am here",
-    author: "Ludo Anderson",
-    text: "Lorem ipsum dolor sit amet",
-    type: "location"
-
-  }, {
-    date: new Date(),
-    title: "For my friends",
-    author: "Sara Orwell",
-    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
-    type: "text"
-
-  }]
 }
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -6,7 +6,42 @@ import { Component } from '@angular/core';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
+    data: Boolean = true
+  
+    timeline = [{
+      date: new Date(),
+      title: "1973",
+      author: "John Mybeweeg",
+      text: "The Department was started by introducing PUC (Natural Science, Physics and Chemistry) in 1973.",
+      type: "picture"
+    }, {
+      date: new Date(),
+      title: "1979 and 1984",
+      author: "Miranda Smith",
+      text: "The UG Programme in Botany was started in the year 1979 and upgraded to PG level in the year, 1984 in order to meet the demands of Botany in higher studies.",
+      type: "video"
 
-  constructor() {}
+    }, {
+      date: new Date(),
+      title: "1986",
+      author: "Ludo Anderson",
+      text: "The substantial availability of staff members with Doctorate in the department has lead to establish Research programmes (M. Phil and Ph. D) in Botany in the year, 1986.",
+      type: "location"
 
+    }, {
+      date: new Date(),
+      title: "2001-2002",
+      author: "Sara Orwell",
+      text: "In the year, 2001-2002, the Branch V Botany (UG) was renamed as Plant Biology and Plant Biotechnology under the branch Va",
+      type: "text"
+  }]
+
+  constructor() {
+  }
+
+  ionViewWillEnter() {
+    setTimeout(()=>{
+      this.data = false
+    }, 2000);
+  }
 }
